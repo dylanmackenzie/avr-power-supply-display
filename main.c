2479903478aa06072f19b1b85e8037fd8072ca36
Dylan MacKenzie <dylanxmackenzie@gmail.com>
2014-11-06 15:26:00 -0800

    make adc selector a button and add state led
    fix comment style

--- main.c
+++ main.c
@@ -7,10 +7,12 @@
 
 #define CHANGE_RAIL_MSG_TIME 60
 #define BOOT_MSG_TIME 100
+#define DEBOUNCE_TIME 100
 
 #define ADC_PLUS_PIN 1
 #define ADC_MINUS_PIN 2
 #define ADC_SELECT_PIN 0
+#define ADC_LED_PIN 3
 #define DIGIT_CONTROL_MASK 0xf0 // pins responsible for selecting digits
 #define MUX_MASK 0x0f           // bits in ADMUX responsible for selecting input
 
@@ -38,7 +40,7 @@ static inline void initADC(void)
 static inline void initIO(void)
 {
   // low: inputs, high: outputs
-  DDRA = DIGIT_CONTROL_MASK;
+  DDRA = DIGIT_CONTROL_MASK | _BV(ADC_LED_PIN);
   DDRB = 0xff;
   PORTA |= _BV(ADC_SELECT_PIN); // enable pull-up resistor on select pin
 }
@@ -65,7 +67,10 @@ int main(void)
 
   uint8_t msg_timer = BOOT_MSG_TIME; // counter for message display time
   uint8_t dp = 0x00; // decimal point position
-  uint8_t old_ap = 0; // old analog pin
+  uint8_t adc_pin = 1; // newly selected adc pin (1 is ADC_PLUS_PIN)
+  uint8_t adc_pin_state = 1; // currently selected adc pin (1 is ADC_PLUS_PIN)
+  uint8_t adc_btn_state = _BV(ADC_SELECT_PIN); // state of adc select button
+  uint8_t dbnc_timer = 0;
 
   cli();
   initADC();
@@ -73,26 +78,46 @@ int main(void)
   initTimer();
   sei();
 
-  old_ap = PINA & _BV(ADC_SELECT_PIN); // initialize the select pin state
-
   // main loop
   for (;;) {
-    if (!( ADCSRA & _BV(ADSC) )) { // we have a new adc reading
+
+    // reading of the adc button differs from button state
+    if ((PINA & _BV(ADC_SELECT_PIN)) ^ adc_btn_state) {
+
+      // reading has been consistent so we have a state change
+      if (--dbnc_timer == 0) {
+        adc_btn_state ^= _BV(ADC_SELECT_PIN);
+
+        // button is pressed down
+        if (adc_btn_state == 0) {
+          adc_pin ^= 1;
+        }
+
+        dbnc_timer = DEBOUNCE_TIME;
+      }
+    } else {
+      dbnc_timer = DEBOUNCE_TIME;
+    }
+
+    // if we have a new adc reading
+    if (!( ADCSRA & _BV(ADSC) )) {
       acc_cnt += 1;
       acc += ADC; // add the reading to our accumulator
 
-      if (acc_cnt == 16) { // we have 16 adc readings
+      // if we have 16 adc readings
+      if (acc_cnt == 16) {
         acc_cnt = 0;
         acc >>= 2; // divide by 4 for oversampling
 
-        // Do an exponentially weighted moving average with a factor of 8
+        // do an exponentially weighted moving average with a factor of 8
         old_acc = acc + old_acc - (old_acc - 8/2) / 8;
         acc = old_acc / 8;
 
-        acc *= 12; // multiply by 12/32 to scale 2^12 to 1500 without overflowing a uint16_t
+        // multiply by 12/32 to scale 2^12 to 1500 without overflowing a uint16_t
+        acc *= 12;
         acc >>= 5;
 
-        // we are not displaying a message
+        // if we are not displaying a message
         if (msg_timer == 0) {
           bin_to_bcd(acc, digits);
           dp = _BV(1);
@@ -100,24 +125,26 @@ int main(void)
           msg_timer--;
         }
 
-        // select pin has changed
-        if ((PINA & _BV(ADC_SELECT_PIN)) ^ old_ap) {
-          old_ap ^= _BV(ADC_SELECT_PIN); // toggle stored select pin
+        // if button has been pressed
+        if (adc_pin != adc_pin_state) {
+          adc_pin_state = adc_pin;
           msg_timer = CHANGE_RAIL_MSG_TIME; // display message
           dp = 0x00;
           digits[0] = 10; // \0
           ADMUX &= ~MUX_MASK;
 
-          if (old_ap) {
+          if (adc_pin) {
             ADMUX |= ADC_PLUS_PIN & MUX_MASK;
             digits[1] = 11; // 'p'
             digits[2] = 12; // 'o'
             digits[3] = 13; // 's'
+            PORTA |= _BV(ADC_LED_PIN);
           } else {
             ADMUX |= ADC_MINUS_PIN & MUX_MASK;
             digits[1] = 14; // 'n'
             digits[2] = 15; // 'e'
             digits[3] = 16; // 'g'
+            PORTA &= ~_BV(ADC_LED_PIN);
           }
         }
 
